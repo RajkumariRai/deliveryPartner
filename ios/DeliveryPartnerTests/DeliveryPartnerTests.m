f2a7f12d55008828efebe8a6f1c87c2123d3ee0f
Ankurdhamapadhan <ankurdhamapadhan@gmail.com>
2022-05-28 12:07:37 +0530

    ankur

--- ios/DeliveryPartnerTests/DeliveryPartnerTests.m
+++ ios/DeliveryPartnerTests/DeliveryPartnerTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface AwesomeProjectTests : XCTestCase
+@interface DeliveryPartnerTests : XCTestCase
 
 @end
 
-@implementation AwesomeProjectTests
+@implementation DeliveryPartnerTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
