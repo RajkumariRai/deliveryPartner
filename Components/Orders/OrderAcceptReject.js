d2e7c4366cdeacf2f585741e0eb6259c3c4b967c
Ankurdhamapadhan <ankurdhamapadhan@gmail.com>
2022-06-02 15:53:00 +0530

    ankur

--- Components/Orders/OrderAcceptReject.js
+++ Components/Orders/OrderAcceptReject.js
@@ -5,7 +5,6 @@ import {button} from '../CommonStyles/Index';
 import RejectOrderDailog from './RejectOrderDailog';
 
 const OrderAcceptReject = props => {
-  console.log('props=======', props);
   const {params} = props.route;
   const [active, setActive] = useState(2);
   const [rejectDailog, setRejectDailog] = useState(false);
@@ -24,117 +23,117 @@ const OrderAcceptReject = props => {
     <View>
       <ScrollView>
         <View style={styles.orderAccpectMainView}>
-          <View style={styles.borderColorset}>
-            <View style={styles.borderwitdhTop}>
-              <Text style={styles.orderIdcenter}>ORD123456789</Text>
-            </View>
-            <View style={{padding: 20}}>
-              <View>
-                <Text style={styles.labelColor}>Order</Text>
-                <View style={styles.borderColorTotalcount}>
-                  <View style={styles.borderbottomfororder}>
-                    {/* map section */}
-                    <View>
-                      <View style={styles.flexSpace}>
-                        <Text style={styles.boxinnerColor}>Deal 1</Text>
-                        <Text style={styles.boxinnerColor}>350</Text>
-                      </View>
-                      <View style={styles.flexSpace}>
-                        <Text style={styles.boxinnerColor}>burger</Text>
-                        <Text style={styles.boxinnerColor}>70</Text>
-                      </View>
-                      <View style={styles.flexSpace}>
-                        <Text style={styles.boxinnerColor}>
-                          7up regular 25ml
-                        </Text>
-                        <Text style={styles.boxinnerColor}>60</Text>
-                      </View>
-                      <View style={styles.flexSpace}>
-                        <Text style={styles.boxinnerColor}>
-                          Delivery charge
-                        </Text>
-                        <Text style={styles.boxinnerColor}>40</Text>
-                      </View>
+          <View style={button.commonBackgroundColor}>
+            <Text style={styles.orderIdcenter}>ORD123456789</Text>
+          </View>
+          <View style={{padding: 20}}>
+            <View>
+              <Text style={[styles.labelColor, button.commonColor]}>Order</Text>
+              <View style={styles.borderColorTotalcount}>
+                <View style={styles.borderbottomfororder}>
+                  {/* map section */}
+                  <View>
+                    <View style={styles.flexSpace}>
+                      <Text style={styles.boxinnerColor}>Deal 1</Text>
+                      <Text style={styles.boxinnerColor}>350</Text>
+                    </View>
+                    <View style={styles.flexSpace}>
+                      <Text style={styles.boxinnerColor}>burger</Text>
+                      <Text style={styles.boxinnerColor}>70</Text>
+                    </View>
+                    <View style={styles.flexSpace}>
+                      <Text style={styles.boxinnerColor}>7up regular 25ml</Text>
+                      <Text style={styles.boxinnerColor}>60</Text>
+                    </View>
+                    <View style={styles.flexSpace}>
+                      <Text style={styles.boxinnerColor}>Delivery charge</Text>
+                      <Text style={styles.boxinnerColor}>40</Text>
                     </View>
                   </View>
-                  <View style={[styles.flexSpace, {paddingTop: 5}]}>
-                    <Text style={styles.totalamountBold}>Total</Text>
-                    <Text style={styles.totalamountBold}>520</Text>
-                  </View>
+                </View>
+                <View style={[styles.flexSpace, {paddingTop: 5}]}>
+                  <Text style={styles.totalamountBold}>Total</Text>
+                  <Text style={styles.totalamountBold}>520</Text>
                 </View>
               </View>
-              <View>
-                <Text style={styles.labelColor}>Location</Text>
-                <View style={styles.borderColorTotalcount}>
-                  <Text style={styles.boxinnerColor}>
-                    Location : 11/B Vshu surat 45345345
-                  </Text>
-                </View>
-              </View>
-              <View>
-                <Text style={styles.labelColor}>Customer</Text>
-                <View style={styles.borderColorTotalcount}>
-                  <Text style={styles.boxinnerColor}>Name : Vishal Mistry</Text>
-                  <Text style={styles.boxinnerColor}>
-                    Phone : +91 435345345353
-                  </Text>
-                </View>
-              </View>
-              <View>
-                <Text style={styles.labelColor}>Payment</Text>
-                <View style={styles.borderColorTotalcount}>
-                  <View style={{flexDirection: 'row'}}>
-                    <Text style={styles.boxinnerColor}>Payment</Text>
-                    <Text style={[styles.boxinnerColor, {marginLeft: '20%'}]}>
-                      Online
-                    </Text>
-                  </View>
-                </View>
-              </View>
-              <View>
-                <Text style={styles.timeHeadling}>Time :</Text>
-                <Text style={styles.timeValueColor}>11:30 AM</Text>
-              </View>
-
-              {!params && (
-                <View style={[styles.flexSpace, {paddingTop: 10}]}>
-                  <Button
-                    mode={active == 1 ? 'contained' : 'outlined'}
-                    color={active == 1 ? '' : 'rgb(211, 211, 211)'}
-                    style={
-                      active == 1 ? button.commonButton : styles.inactiveButton
-                    }
-                    onPress={() => {
-                      rejectHandle();
-                    }}>
-                    Reject
-                  </Button>
-                  <Button
-                    mode={active == 2 ? 'contained' : 'outlined'}
-                    color={active == 2 ? '' : 'rgb(211, 211, 211)'}
-                    style={
-                      active == 2 ? button.commonButton : styles.inactiveButton
-                    }
-                    onPress={() => {
-                      acceptHandle();
-                    }}>
-                    Accept
-                  </Button>
-                </View>
-              )}
-              {params === 'Active' && (
-                <View style={{paddingTop: 10, alignItems: 'center'}}>
-                  <Button
-                    mode="contained"
-                    style={button.commonButton}
-                    onPress={() => {
-                      // StartOrderHandle();
-                    }}>
-                    Start
-                  </Button>
-                </View>
-              )}
             </View>
+            <View>
+              <Text style={[styles.labelColor, button.commonColor]}>
+                Location
+              </Text>
+              <View style={styles.borderColorTotalcount}>
+                <Text style={styles.boxinnerColor}>
+                  Location : 11/B Vshu surat 45345345
+                </Text>
+              </View>
+            </View>
+            <View>
+              <Text style={[styles.labelColor, button.commonColor]}>
+                Customer
+              </Text>
+              <View style={styles.borderColorTotalcount}>
+                <Text style={styles.boxinnerColor}>Name : Vishal Mistry</Text>
+                <Text style={styles.boxinnerColor}>
+                  Phone : +91 435345345353
+                </Text>
+              </View>
+            </View>
+            <View>
+              <Text style={[styles.labelColor, button.commonColor]}>
+                Payment
+              </Text>
+              <View style={styles.borderColorTotalcount}>
+                <View style={{flexDirection: 'row'}}>
+                  <Text style={styles.boxinnerColor}>Payment</Text>
+                  <Text style={[styles.boxinnerColor, {marginLeft: '20%'}]}>
+                    Online
+                  </Text>
+                </View>
+              </View>
+            </View>
+            <View>
+              <Text style={styles.timeHeadling}>Time :</Text>
+              <Text style={styles.timeValueColor}>11:30 AM</Text>
+            </View>
+
+            {!params && (
+              <View style={[styles.flexSpace, {paddingTop: 10}]}>
+                <Button
+                  mode={active == 1 ? 'contained' : 'outlined'}
+                  color={active == 1 ? '' : 'rgb(211, 211, 211)'}
+                  style={
+                    active == 1 ? button.commonButton : styles.inactiveButton
+                  }
+                  onPress={() => {
+                    rejectHandle();
+                  }}>
+                  Reject
+                </Button>
+                <Button
+                  mode={active == 2 ? 'contained' : 'outlined'}
+                  color={active == 2 ? '' : 'rgb(211, 211, 211)'}
+                  style={
+                    active == 2 ? button.commonButton : styles.inactiveButton
+                  }
+                  onPress={() => {
+                    acceptHandle();
+                  }}>
+                  Accept
+                </Button>
+              </View>
+            )}
+            {params === 'Active' && (
+              <View style={{paddingTop: 10, alignItems: 'center'}}>
+                <Button
+                  mode="contained"
+                  style={button.commonButton}
+                  onPress={() => {
+                    // StartOrderHandle();
+                  }}>
+                  Start
+                </Button>
+              </View>
+            )}
           </View>
         </View>
       </ScrollView>
@@ -152,17 +151,6 @@ const styles = StyleSheet.create({
     flexGrow: 1,
     backgroundColor: '#fff',
   },
-  borderColorset: {
-    borderColor: '#d60e0e',
-    borderWidth: 1,
-    margin: 10,
-    borderRadius: 20,
-  },
-  borderwitdhTop: {
-    backgroundColor: '#d60e0e',
-    borderTopLeftRadius: 20,
-    borderTopRightRadius: 20,
-  },
   orderIdcenter: {
     textAlign: 'center',
     fontWeight: 'bold',
@@ -172,7 +160,6 @@ const styles = StyleSheet.create({
     paddingTop: 5,
   },
   labelColor: {
-    color: '#d60e0e',
     fontSize: 15,
     fontWeight: '500',
   },
@@ -217,7 +204,6 @@ const styles = StyleSheet.create({
   },
   inactiveButton: {
     backgroundColor: '#fff',
-    color: '#26ae61',
     borderRadius: 30,
     fontWeight: '700',
     paddingLeft: 20,
