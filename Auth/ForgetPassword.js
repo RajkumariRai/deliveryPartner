6f53ad0216cdd1504f6752d3d2b4f9c249d31b35
Ankurdhamapadhan <ankurdhamapadhan@gmail.com>
2022-06-08 13:47:49 +0530

    ankur

--- Auth/ForgetPassword.js
+++ Auth/ForgetPassword.js
@@ -2,6 +2,7 @@ import React, {useState} from 'react';
 import {StyleSheet, Text, View, Image, TouchableOpacity} from 'react-native';
 import {HelperText, TextInput, Button} from 'react-native-paper';
 import {button} from '../Components/CommonStyles/Index';
+import Icon from 'react-native-vector-icons/dist/FontAwesome';
 
 let error = false;
 const validateData = () => {
@@ -27,29 +28,49 @@ export default function ForgetPassword(props) {
 
   return (
     <View style={styles.loginMainView}>
+      <View style={{position: 'absolute', top: 0}}>
+        <View style={{flexDirection: 'row', padding: 10}}>
+          <View>
+            <TouchableOpacity
+              onPress={() => {
+                navigation.goBack();
+              }}>
+              <Icon name={'arrow-left'} size={16} color="rgb(112,112,112)" />
+            </TouchableOpacity>
+          </View>
+          <View style={{width: '100%'}}>
+            <Text
+              style={[
+                button.commonColor,
+                {textAlign: 'center', fontWeight: '700'},
+              ]}>
+              Forgot Password
+            </Text>
+          </View>
+        </View>
+      </View>
       <View style={styles.image}>
         <Image source={require('../assets/Logo.png')} />
       </View>
-
       <View style={styles.formSectionPadding}>
         <View
           style={{
-            paddingTop: 50,
             paddingRight: 40,
             paddingLeft: 40,
-           
-           
+            paddingBottom: 40,
           }}>
-          <Text style ={{color:"#000", fontSize:15}}>
+          <Text style={styles.forgotpassTitleColor}>
             Please enter your email address.. you will recieve a link to create
             a new password via email
           </Text>
         </View>
-        <View style={{paddingBottom: 15, paddingTop:15}}>
+        <View style={{paddingBottom: 15, paddingTop: 15}}>
           <TextInput
             mode="outlined"
             theme={{roundness: 20}}
-            left={<TextInput.Icon name="email" color={button.commonColor.color} />}
+            left={
+              <TextInput.Icon name="email" color={button.commonColor.color} />
+            }
             style={button.commonTextInput}
             placeholder="Email"
             placeholderTextColor="#000"
@@ -100,6 +121,7 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     flexGrow: 1,
+    backgroundColor: '#fff',
   },
   image: {
     alignItems: 'center',
@@ -107,5 +129,10 @@ const styles = StyleSheet.create({
   },
   formSectionPadding: {
     padding: 15,
+  },
+  forgotpassTitleColor: {
+    color: 'rgb(112,112,112)',
+    fontSize: 12,
+    textAlign: 'center',
   },
 });
